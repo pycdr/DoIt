0bd2b85904d5a58528094629bd4f41f24da1fdfd
mmt <pycdremail@gmail.com>
2020-09-03 19:50:48 +0430

    something edited and update

--- CLI/startup.py
+++ CLI/startup.py
@@ -1,24 +1,28 @@
 import	prompt_toolkit as ptk
 from	prompt_toolkit import HTML, print_formatted_text as print
 from	prompt_toolkit.styles import Style
-import	click
 import	json
+import	argparse
+import	os
 
-style = Style.from_dict(json.loads(open("./style.json"),'r'))
-@click.group
-def opening():
-	pass
-@opening.command
-def welcome():
-	print(HTML("<welcome_message>\
-	welcome to the DoIt program!\
-	</welcome_message>"),style=style)
+style = Style.from_dict(json.loads(open("./style.json",'r').read()))
 
-def recently_opened_show(ctx, param, value):
-	if not value or ctx.resilient_parsing:
-		return
-	temp = open("./recently_opened",'r').read()
-	l = [x.split(",") for x in temp.split("\n")]
-	# coming soon...!
-	clicl.exit()
-@click.option("--recently",is_eager=True,callback=recently_opened_show)
+def show_recently_opened():
+	s = open("./recently_opened",'r').read()
+	for x in s.split("\n"):
+		if x=="" or x=="," or x.isspace():
+			continue
+		l = x.split(",")
+		print(HTML("<time>["+l[1]+"]</time> ~> "+l[0]),style=style)
+parser = argparse.ArgumentParser(description="an app to schedule your life or your work.")
+parser.add_argument("--recently", required=False, action="store_true", help="get recently opened pickles.")
+args = parser.parse_args()
+if args.recently:
+	show_recently_opened()
+	exit()
+parser.add_argument("path", metavar="path", type=str, help="to load data from pickle file.")
+args = parser.parse_args()
+
+print(HTML("<welcome_message>\
+welcome to the DoIt program!\
+</welcome_message>"),style=style)
