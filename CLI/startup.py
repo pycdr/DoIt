ff8bd56d6a0ec193267dd66f0351e240f319acd5
mmt <pycdremail@gmail.com>
2020-09-03 20:36:41 +0430

    debugging a small problem

--- CLI/startup.py
+++ CLI/startup.py
@@ -14,15 +14,44 @@ def show_recently_opened():
 			continue
 		l = x.split(",")
 		print(HTML("<time>["+l[1]+"]</time> ~> "+l[0]),style=style)
+
 parser = argparse.ArgumentParser(description="an app to schedule your life or your work.")
 parser.add_argument("--recently", required=False, action="store_true", help="get recently opened pickles.")
+parser.add_argument("-l", "--list", required=False, action="store_true", help="show list of names for schedule")
+parser.add_argument("--path", required=False, metavar="path", type=str, help="to load data from pickle file(it can be a name).")
 args = parser.parse_args()
+
 if args.recently:
 	show_recently_opened()
 	exit()
-parser.add_argument("path", metavar="path", type=str, help="to load data from pickle file.")
-args = parser.parse_args()
-
+if args.list:
+	d = json.loads(open("./set_path.json", 'r').read())
+	if len(d)==0:
+		print(HTML("<info>oops! there is not any name in list!</info>"),style=style)
+	for x in d:
+		print(HTML("<schname>["+x+"]</schname>: "+d[x]),style=style)
+	exit()
+	
 print(HTML("<welcome_message>\
 welcome to the DoIt program!\
 </welcome_message>"),style=style)
+if args.path:
+	if os.path.isfile(args.path):
+		print("you set path as "+args.path)
+		if not args.path.endswith(".pckl"):
+			print(HTML(
+"<warn>the given path doesn't seem a pickle file\n\
+if you're not sure that it's pickle, we will get an error!\n\
+we suggest you to save our data as pickle(.pckl) file, thanks!</warn>"),style=style)
+	else:
+		d = json.loads(open("./set_path.json", 'r').read())
+		if args.path in d:
+			print("you set path as "+d[args.path])
+			if not d[args.path].endswith(".pckl"):
+				print(HTML(
+"<warn>the given path doesn't seem a pickle file\n\
+if you're not sure that it's pickle, we will get an error!\n\
+we suggest you to save our data as pickle(.pckl) file, thanks!</warn>"),style=style)
+		else:
+			print(HTML("<error>please give an existing pickle file path or saved name!</error>"),style=style)
+			exit()
