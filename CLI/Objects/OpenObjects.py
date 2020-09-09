7c66771b3b0134e46052f50516d7a4dbe7b57ef6
mmt <pycdremail@gmail.com>
2020-09-09 10:23:04 +0430

    Adding popup and Changing type of Boxes to Form and Widgets

--- CLI/Objects/OpenObjects.py
+++ CLI/Objects/OpenObjects.py
@@ -2,49 +2,69 @@ import npyscreen as nps
 import os
 import json
 import curses
+import time
 from curses import KEY_UP,KEY_DOWN,KEY_LEFT,KEY_RIGHT
 
-class RecentlyOpened(nps.BoxTitle):
-	def create(self):
-		self.values = [
-			"["+x.split(",")[1]+"]: "+x.split(",")[0]
-			for x in open("recently_opened",'r').read().split("\n")
-			if(not x=="")and(not x.isspace())
-		]
+t = time.time()
+rclist = [
+	"["+x.split(",")[1]+"]: "+x.split(",")[0]
+	for x in open("recently_opened",'r').read().split("\n")
+	if(not x=="")and(not x.isspace())
+]
 
-class NamesPaths(nps.BoxTitle):
+class RecentlyOpened(nps.ActionForm):
 	def create(self):
+		self.wmain = self.add(
+			nps.TitleSelectOne, name="choose one of the file", value=[],
+			values=rclist, scroll_exit=True
+		)
+	def on_ok(self):
+		pass
+	def on_cancel(self):
+		self.parentApp.setNextForm("MAIN")
+
+class NamesPaths(nps.ActionForm):
+	def create(self):
+		self.box = self.add(nps.BoxTitle, name="list")
 		self.names_dict = json.load(open("set_path.json",'r'))
-		self.values = [
+		self.box.values = [
 			x+" : "+self.names_dict[x]
 			for x in self.names_dict
 		]
+	def on_ok(self):
+		pass
+	def on_cancel(self):
+		self.parentApp.setNextForm("MAIN")
 
-class GetPath(nps.BoxTitle):
-	_contained_widget = nps.Textfield
+class GetPath(nps.ActionForm):
+	def create(self):
+		self.pinput = self.add(nps.Textfield, name="input path") 
+	def on_ok(self):
+		pass
+	def on_cancel(self):
+		self.parentApp.setNextForm("MAIN")
 
-class OpenForm(nps.FormBaseNew):
+class OpenForm(nps.ActionForm):
 	def create(self):
 		y,x = self.useable_space()
-		self.box1 = self.add(
-			RecentlyOpened, name="recently opened files",
-			relx=0, rely=0, 
-			max_width=x//2-1, max_height=y-5
+		self.selecting = self.add(
+			nps.TitleSelectOne, max_height=y-4, value=[],
+			name="how to get path?", values=["show recently opened files", "Select by saved paths", "input path"], 
+			scroll_exit=True)
+	def on_ok(self):
+		if len(self.selecting.value)==0:
+			nps.notify_wait("first, Please select one of the choices!")
+			return
+		self.parentApp.setNextForm(
+			{
+				"show recently opened files": "OPENFORM1",
+				"Select by saved paths": "OPENFORM2",
+				"input path": "OPENFORM3"
+			}[self.selecting.values[
+				self.selecting.value[0]
+			]]
 		)
-		self.box1.create()
-		self.box2 = self.add(
-			NamesPaths, name="saved paths",
-			relx=x//2-1, rely=0,
-			max_width=x//2-1, max_height=y-5
-		)
-		self.box2.create()
-		self.box3 = self.add(
-			GetPath, name="input path",
-			relx=1, rely=-5,
-			max_width=x-3, max_height=3
-		)
-		self.add_event_hander("READINGEVENT", self.cgpath)
-	def while_waiting(self):
-		self.parentApp.queue_event(nps.Event("READINGEVENT"))
-	def cgpath(self,e):
-		pass
+	def on_cancel(self):
+		if nps.notify_yes_no("Do you want to exit", title="Exit",editw=1):
+			nps.notify_wait("Bye..!")
+			exit()
--- CLI/startup.py
+++ CLI/startup.py
@@ -1,9 +1,12 @@
 import npyscreen as nps
-from Objects.OpenObjects import OpenForm
+from Objects.OpenObjects import OpenForm, RecentlyOpened, NamesPaths, GetPath
 
 class App(nps.StandardApp): #or: nps.NPSAppManaged
 	def onStart(self):
 		self.openform = self.addForm("MAIN", OpenForm)
+		self.openformb1 = self.addForm("OPENFORM1", RecentlyOpened)
+		self.openformb2 = self.addForm("OPENFORM2", NamesPaths)
+		self.openformb3 = self.addForm("OPENFORM3", GetPath)
 		self.daysform = None
 		self.dayform =  None
 		self.timeform = None
