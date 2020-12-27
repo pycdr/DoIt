645344859c72a80960d8ce6ab8536564829b6569
mmt <pycdremail@gmail.com>
2020-12-27 20:28:21 +0330

    update this part

--- src/Pages/PageSelector.js
+++ src/Pages/PageSelector.js
@@ -1,13 +1,23 @@
 import React from "react";
-import Year from "./YearPage.js";
+/*import Year from "./YearPage.js";
 import Month from "./MonthPage.js";
 import Day from "./YearPage.js";
-import Time from "./TimePage.js";
+import Time from "./TimePage.js";*/
+import { Collapse, Card, Modal, Space, Image, Button } from "antd";
+import "antd/dist/antd.css";
+import { EyeOutlined } from "@ant-design/icons";
+const { Panel } = Collapse;
 
 class PageSelector extends React.Component {
 	constructor(props){
 		super(props);
 		this.data = props.data;
+		this.state = {
+			y_visibility : {},
+			m_visibility : {},
+			d_visibility : {},
+			t_visibility : {}
+		}
 	}
 	render(){
 		if ("data" in this.data){
@@ -31,7 +41,130 @@ class PageSelector extends React.Component {
 			else if (!Array.isArray(this.data.time)) years = Array(this.data.time);
 			else times = this.data.time;
 			
-			return <>{JSON.stringify(this.data)}</> //temporary
+			console.log(years, months, days, times);
+			
+			return (
+				<Collapse accordion defaultActiveKey={["y"]}>
+					<Panel header="Years" key="y"><Space wrap>
+						{years.map(x => (
+						<Card
+							title={x["@number"]}
+							extra={
+							<Button
+								icon={<EyeOutlined />}
+								onClick={()=>{
+									this.state.y_visibility[x["@number"]] = true;
+									this.setState(this.state);
+								}}
+								>
+								show
+							</Button>
+							}
+						>
+							<Modal
+								title={"detail for year "+x["@number"]}
+								visible={this.state.y_visibility[x["@number"]]}
+								width="90vw"
+								onOk={()=>{
+									this.state.y_visibility[x["@number"]] = false;
+									this.setState(this.state);
+								}}
+								onCancel={()=>{
+									this.state.y_visibility[x["@number"]] = false;
+									this.setState(this.state);
+								}}
+							>
+							{Object.keys(x).map(y => {
+								if (y[0]!=="@") return <PageSelector data={{month:x[y]}} />
+							})}
+							</Modal>
+							<Image src={x["@image"]} height="15vw"/>
+						</Card>
+						))}
+					</Space></Panel>
+					
+					
+					<Panel header="Months" key="m"><Space wrap>
+						{months.map(x => (
+						<Card
+							title={x["@number"]}
+							extra={
+							<Button
+								icon={<EyeOutlined />}
+								onClick={()=>{
+									this.state.m_visibility[x["@number"]] = true;
+									this.setState(this.state);
+								}}
+								>
+								show
+							</Button>
+							}
+						>
+							<Modal
+								title={"detail for month "+x["@number"]}
+								visible={this.state.m_visibility[x["@number"]]}
+								width="80vw"
+								onOk={()=>{
+									this.state.m_visibility[x["@number"]] = false;
+									this.setState(this.state);
+								}}
+								onCancel={()=>{
+									this.state.m_visibility[x["@number"]] = false;
+									this.setState(this.state);
+								}}
+							>
+							{Object.keys(x).map(y => {
+								if (y[0]!=="@") return <PageSelector data={{day:x[y]}} />
+							})}
+							</Modal>
+							<Image src={x["@image"]} height="15vw"/>
+						</Card>
+						))}
+					</Space></Panel>
+					
+					
+					<Panel header="Days" key="d"><Space wrap>
+						{days.map(x => (
+						<Card
+							title={x["@number"]}
+							extra={
+							<Button
+								icon={<EyeOutlined />}
+								onClick={()=>{
+									this.state.d_visibility[x["@number"]] = true;
+									this.setState(this.state);
+								}}
+								>
+								show
+							</Button>
+							}
+						>
+							<Modal
+								title={"detail for day "+x["@number"]}
+								visible={this.state.d_visibility[x["@number"]]}
+								width="70vw"
+								onOk={()=>{
+									this.state.d_visibility[x["@number"]] = false;
+									this.setState(this.state);
+								}}
+								onCancel={()=>{
+									this.state.d_visibility[x["@number"]] = false;
+									this.setState(this.state);
+								}}
+							>
+							{JSON.stringify(x)} //temporary
+							</Modal>
+							<Image src={x["@image"]} height="15vw"/>
+						</Card>
+						))}
+					</Space></Panel>
+					
+					
+					<Panel header="Times" key="t">
+					
+					</Panel>
+				</Collapse>
+			)
 		}
 	}
 }
