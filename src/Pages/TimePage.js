dd9506eb657d9ca14259a7ce63dff4ab42f49b76
mmt <pycdremail@gmail.com>
2020-12-27 22:50:56 +0330

    Add a template code to the TimePage.js

---
+++ src/Pages/TimePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class ShowTimes extends React.Component {
+	constructor(props){
+		super(props);
+	}
+	render(){
+		return <></>; //temporary
+	}
+}
+
+export default ShowTimes;
